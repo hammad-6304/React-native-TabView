1105ccc868831c2ecd141de07474a3ec56cc801d
Hammad-6304 <103407647+hammad-6304@users.noreply.github.com>
2022-11-12 16:55:40 +0500

    updated

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
 import SafeAreaViewHoc from "../HOC/SafeAreaViewHoc";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import AccountScreen from "./AccountScreen";
 
-const ChatScreen = () => (
+const InboxScreen = SafeAreaViewHoc(() => (
   <View>
-    <Text>Chat Screen</Text>
+    <Text style={{ fontSize: 28, color: "teal" }}>Inbox Screen</Text>
   </View>
+));
+
+const ChatTopTabs = createMaterialTopTabNavigator();
+const ChatScreen = () => (
+  <ChatTopTabs.Navigator>
+    <ChatTopTabs.Screen name="inbox" component={InboxScreen} />
+    <ChatTopTabs.Screen name="sentAccount" component={AccountScreen} />
+  </ChatTopTabs.Navigator>
+  // <InboxScreen/>
 );
 
 export default SafeAreaViewHoc(ChatScreen);
